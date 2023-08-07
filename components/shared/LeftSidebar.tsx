d8072baa8a9829ab3447ac83f48dc5ca5a503452
guilherhenri <guilherhenri12@gmail.com>
2023-08-07 07:55:14 -0300

    Add padding to sign-out section

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -36,7 +36,7 @@ function LeftSidebar() {
         })}
       </div>
 
-      <div className="mt-10">
+      <div className="mt-10 px-6">
         <SignedIn>
           <SignOutButton signOutCallback={() => router.push('/sign-in')}>
             <div className="flex cursor-pointer gap-4 p-4">
