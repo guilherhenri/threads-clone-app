664a097885172c37dc55eebba5367d435e9a5cd7
guilherhenri <guilherhenri12@gmail.com>
2023-08-08 08:55:18 -0300

    Add fetchThreadById action

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -19,9 +19,9 @@ export async function createThread({
   communityId,
   path,
 }: CreateThreadParams) {
-  try {
-    connectToDB()
+  connectToDB()
 
+  try {
     const thread = await Thread.create({
       text,
       author,
@@ -68,4 +68,41 @@ export async function fetchPosts(pageNumber = 1, pageSize = 20) {
   const isNext = totalPostsCount > skipAmount + posts.length
 
   return { posts, isNext }
+}
+
+export async function fetchThreadById(id: string) {
+  connectToDB()
+
+  try {
+    const thread = await Thread.findById(id)
+      .populate({
+        path: 'author',
+        model: User,
+        select: '_id id name image',
+      })
+      .populate({
+        path: 'children',
+        populate: [
+          {
+            path: 'author',
+            model: User,
+            select: '_id id name parentId image',
+          },
+          {
+            path: 'children',
+            model: Thread,
+            populate: {
+              path: 'author',
+              model: User,
+              select: '_id id name parentId image',
+            },
+          },
+        ],
+      })
+      .exec()
+
+    return thread
+  } catch (error: any) {
+    throw new Error(`Error fetching thread: ${error.message}`)
+  }
 }
