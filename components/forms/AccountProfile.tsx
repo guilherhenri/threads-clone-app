d8d7f691742367a530241038c9ac5f690fa624d5
guilherhenri <guilherhenri12@gmail.com>
2023-08-08 07:47:28 -0300

    Create PostThread component

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -14,6 +14,7 @@ import {
   FormField,
   FormItem,
   FormLabel,
+  FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { UserValidation } from '@/lib/validations/user'
@@ -145,6 +146,8 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                   onChange={(e) => handleImage(e, field.onChange)}
                 />
               </FormControl>
+
+              <FormMessage />
             </FormItem>
           )}
         />
@@ -165,6 +168,8 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                   {...field}
                 />
               </FormControl>
+
+              <FormMessage />
             </FormItem>
           )}
         />
@@ -185,6 +190,8 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                   {...field}
                 />
               </FormControl>
+
+              <FormMessage />
             </FormItem>
           )}
         />
@@ -205,6 +212,8 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                   {...field}
                 />
               </FormControl>
+
+              <FormMessage />
             </FormItem>
           )}
         />
